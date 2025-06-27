883a7b2c444bb9723a946fbb39e5d1a2c70f8cb7
Babji1999 <domanababji94289@gmail.com>
2025-06-27 07:34:48 +0530

    special vars

--- 03-conditions.sh
+++ 03-conditions.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "error: please run this script with root access"
+    exit 1
+else 
+    echo "you are running with root access"
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]
+    then 
+        echo "installing $2 is ----success"
+    else
+        echo "installing $2 is ''''' failure"
+        exit 1
+    fi    
+}
+
+dnf list installed ngnix
+   if [ $? -ne 0 ]
+   then
+        echo "nignix is not installed"
+        dnf install nginx -y
+        VALIDATE $? "nginx"
+   else
+
+   echo "nginx is already installed"
+fi
